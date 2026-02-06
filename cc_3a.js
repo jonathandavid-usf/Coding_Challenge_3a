6bca6bd76817071c11710aab6cd4cd498f8873f2
Jonathan Schrum <jschrum@usf.edu>
2026-02-06 18:11:17 -0500

    first commit

---
+++ cc_3a.js
@@ -0,0 +1,14 @@
+let customers = [
+    { name: "Lebron James", email: "Lebron@james.com", purchases: ["headband"] },
+    { name: "Michael Jordan", email: "Michael@jordan.com", purchases: ["basketball shoes"] },
+    { name: "Kobe Bryant", email: "Kobe@bryant.com", purchases: ["helicopter tickets"] }
+]
+console.log(customers)
+customers.push({ name: "Kevin Durant", email: "Kevin@durant.com", purchases: ["seat cushion"] })
+console.log(customers)
+customers.shift()
+customers[1].email = "Kobe24@bryant.com"
+customers[1].purchases.push("basketball shorts")
+customers.forEach(customer => {console.log('name: ' + customer.name + ', email: ' + customer.email + ', purchases: ' + customer.purchases)})
+
+
